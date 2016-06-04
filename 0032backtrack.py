42e011d143a0a6b0dbb64ef3a0b19ac0d0840b31
setogen <springh230330kz@gmail.com>
2016-06-04 14:47:53 +0000

    2016-06-05

--- 0032backtrack.py
+++ 0032backtrack.py
@@ -5,37 +5,74 @@
 2==load of queen move
 '''
 board=[[0 for i in range(8)] for j in range(8)]
-queen=[7,6,5,4,3]
-def backtrack(n,m=0):
-	for i in range(8):
-		pass
+bl=len(board)
+queen=[]
+answer=[]
+def backtrack(n):
+	global board
+	global queen
+	global answer
+	global bl
+
+	for i in range(bl):
+		if board[i][n]==0:
+			put_queen()
+			queen_move()
+			#print(n,i)
+			queen.append(i)
+			print(queen)
+			put_queen()
+			queen_move()
+			print_board()
+			if n == bl-1:
+				answer.append(queen)
+				#print(answer)
+				queen.pop()
+				break
+			else:
+				backtrack(n+1)
+	if queen:
+		queen.pop()
+	put_queen()
+	queen_move()
+				
+			
 		
 def put_queen():
 	global board
-	board=[[0 for i in range(8)] for j in range(8)]
+	global bl
+	board=[[0 for i in range(bl)] for j in range(bl)]
 	for i in range(len(queen)):
 		board[queen[i]][i]=1
 		
 def queen_move():
-	for i in range(len(board)):
-		for j in range(len(board)):
+	global board
+	global bl
+	for i in range(bl):
+		for j in range(bl):
 			if board[i][j]==1:
-				for k in range(j+1,len(board)):
+				for k in range(j+1,bl):
 					if board[i][k]==0:
 						board[i][k]=2
-				for k in range(min(i,len(board)-j-1)):
+				for k in range(min(i,bl-j-1)):
 					if board[i-(k+1)][j+(k+1)]==0:
 						board[i-(k+1)][j+(k+1)]=2
-				for k in range(1,min(len(board)-i,len(board)-j)):
+				for k in range(1,min(bl-i,bl-j)):
 					if board[i+k][j+k]==0:
 						board[i+k][j+k]=2
 					
 
 
 def print_board():
-	for i in range(len(board)):
+	print("-----------------")
+	for i in range(bl):
 		print(board[i])
-		
+	print("-----------------")
+
+'''
 put_queen()
 queen_move()
 print_board()
+'''
+backtrack(0)
+print(answer)
